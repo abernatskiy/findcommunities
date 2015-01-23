582092fdcd97a6cce1606ebc08000a5eb0b751a0
Anton Bernatskiy <abernats@uvm.edu>
2015-01-22 22:10:25 -0500

    Now massQ.sh supports passing the required named pipes names as arguments

--- massQ.sh
+++ massQ.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-TXT="./txt"
-BIN="./bin"
-OUT="./out"
+# Usage:
+# massQ.sh [<txtpipe> <binpipe>]
+# Takes network genomes in evs format as its standard input, produces a modularity score and writes it into
+# the standard output.
+# Must be run at the findcommunities directory.
+# Requires two named pipes to work - one for intermediate text formst and one for intermediate binary format.
+# Unless explicitly specified, will use "./txt" and "./bin" for these purposes.o
+
+if [ $# -eq 2 ]; then
+	TXT="$1"
+	BIN="$2"
+elif [ $# -eq 0 ]; then
+	TXT="./txt"
+	BIN="./bin"
+else
+  echo Wrong number of arguments
+  exit 1
+fi
 
 COMMDIR="./Community_latest/"
 COMMCONV="${COMMDIR}convert"
